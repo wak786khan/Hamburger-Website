6be3f3f477503851c2f40a5ea1702053ff93addf
waseem khan <162899951+wak786khan@users.noreply.github.com>
2024-09-25 16:57:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// burger = document.querySelector('.burger')
+// navbar = document.querySelector('.navbar')
+// navlist = document.querySelector('.nav-list')
+// rightnav = document.querySelector('.rightnav')
+
+// burger.addEventLitener('clcik', ()=>{
+//   rightnav.classLis.toggle('v-class-resp');
+//   nav-list.classLis.toggle('v-class-resp')
+//   navbar.classLis.toggle('h-nav-resp') 
+// })
